ef12603073c374c49629f0c5b4a8eb961f0eee14
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-08-02 22:51:35 -0700

    Complete Project Part 2: balancedParentheses problem

--- src/BalancedBrackets.java
+++ src/BalancedBrackets.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Stack;
+
 /*
 #1 Balanced Parentheses
 Write a function that takes in a string and returns true or false
@@ -12,6 +15,23 @@ like curly brackets, square brackets, or angle brackets.
 
 Similar problem: https://www.codewars.com/kata/52774a314c2333f0a7000688/
 
+#2
+Write a function that takes in a string and returns true or false
+based on whether the string’s opening-and-closing marks
+(that is, brackets) are balanced. Account for the following bracket types:
+Parentheses: (  )
+Curly Braces: { }
+Square Brackets: [ ]
+Angle Brackets: < >
+
+The string doesn't need to have all four types of brackets,
+but if an open bracket appears, the pair should also be closed correctly.
+Assume you can use any libraries in Java needed.
+
+Similar problem: https://leetcode.com/problems/valid-parentheses/
+
+#3
+
 
  */
 public class BalancedBrackets {
@@ -22,6 +42,19 @@ public class BalancedBrackets {
         System.out.println(balancedParentheses("())"));
         System.out.println(balancedParentheses("32423(table)")); //true
         System.out.println(balancedParentheses("(java))2432")); //false
+
+        System.out.println();
+        System.out.println(balancedBrackets("()"));
+        System.out.println(balancedBrackets(")"));
+        System.out.println(balancedBrackets("("));
+        System.out.println(balancedBrackets("())"));
+        System.out.println(balancedBrackets("32423(table)")); //true
+        System.out.println(balancedBrackets("(java))2432")); //false
+        System.out.println();
+        System.out.println(balancedBrackets("(}")); //false
+        System.out.println(balancedBrackets("()[]{}")); //true
+        System.out.println(balancedBrackets("<]")); //false
+        System.out.println(balancedBrackets("{<()>[{{}}]()<>}")); //true
     }
 
     private static boolean balancedParentheses(String str) {
@@ -38,5 +71,30 @@ public class BalancedBrackets {
             }
         }
         return closeCounter - openCounter == 0;
+    }
+
+    private static boolean balancedBrackets(String str) {
+        HashMap<Character, Character> hm = new HashMap<>();
+        hm.put(')', '(');
+        hm.put('}', '{');
+        hm.put(']', '[');
+        hm.put('>', '<');
+
+        Stack<Character> stack = new Stack<>();
+        char ch;
+
+        for (int i = 0; i < str.length(); i++) {
+            ch = str.charAt(i);
+            if (hm.containsValue(ch)) { //open
+                stack.add(ch);
+            } else if (hm.containsKey(ch)) { //close
+                if (stack.isEmpty()) {
+                    return false;
+                } else if (hm.get(ch) != stack.pop()) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
     }
 }
